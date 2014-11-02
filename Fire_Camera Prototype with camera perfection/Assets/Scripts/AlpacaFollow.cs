6a045e7f2902c848699ec8e9271d62b434c3973d
stfueuan <professionalmutineers@gmail.com>
2014-11-02 23:26:55 +0000

    Fire Camera Prototype with Working Camera
    Alpaca model added
    Alpaca audio added
    Camera fixed and is perfection :)

---
+++ Fire_Camera Prototype with camera perfection/Assets/Scripts/AlpacaFollow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlpacaFollow : MonoBehaviour {
+
+	private bool following;
+	public GameObject leader;
+	private float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 5.0f;
+		following = false;
+		leader = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (following) 
+		{
+			float step = Time.deltaTime * speed;
+			transform.position = Vector3.MoveTowards (transform.position, leader.transform.position, step);
+		}
+	}
+
+	void OnTriggerEnter(Collider target) 
+	{
+		if (target.tag == "Player") 
+		{
+			following = false;
+			audio.Play();
+		}
+	}
+
+	void OnTriggerExit(Collider target) 
+	{
+		if (target.tag == "Player") 
+		{
+			following = true;
+		}
+	}
+}
---
+++ Fire_Camera Prototype with camera perfection/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour {
+
+	public GUIText scoreText;
+	private int score;
+
+	void Start() 
+	{
+		score = 0;
+		scoreText.text = "Booty: " + score.ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider target) 
+	{
+		if (target.tag == "Pickup") 
+		{
+			target.gameObject.SetActive (false);
+			score += 1;
+			scoreText.text = "Booty: " + score.ToString();
+		}
+	}
+}
